54b38e90fe39091bf33fc40b5bceb093a4ffc822
Ashishk11111 <ashishkashyap72495@gmail.com>
2025-06-15 22:38:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img className='bookmark' src="./bookmark.png" alt="" />
           <h2>DTC And Cluster Bus Pass</h2>
           {/* <hr/> */}
-          <img className='passport' src="/photo.jpg" alt="" width={180}/>
-          <p className='sameP'>ASHISH</p>
+          <img className='passport' src="/photo.jpg" alt="" width={200} height={180}/>
+          <p className='sameP'>ASHISH KASHYAP</p>
           <p className='sameP'>Student All route AC/Non AC</p>
           
           <span className='pass'>
@@ -27,11 +27,11 @@ function App() {
 
           </span>
 
-          <p className='redP'>Valid from 10/02/2025 to 9/7/2025</p>
+          <div className='redP'>Valid from 10/02/2025 to 9/7/2025</div>
 
           <div className='foot-img'>
             <div className='sign-img'><img src="./sign.png" alt="" width={80} /></div>
-            <div className='qr-img'><img src="./qr.png" alt="" width={80} /></div>
+            <div className='qr-img'><img src="./qr.png" alt="" width={70} height={70} /></div>
           </div>
           
         </div>
